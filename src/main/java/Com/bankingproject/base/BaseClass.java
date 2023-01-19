56041dd023941c106e460f85ab8d4de262d66414
AnjuThakare <anju.thakare1989@gmail.com>
2023-01-18 22:28:42 -0500

    DGTL-103:Done Base class Change

--- src/main/java/Com/bankingproject/base/BaseClass.java
+++ src/main/java/Com/bankingproject/base/BaseClass.java
@@ -11,11 +11,17 @@ public class BaseClass {
 		WebDriver driver=new ChromeDriver();       
 		 driver.get("https://demo.guru99.com/V1/index.php");
 		 
+		 
 		
+		 driver.get("https://demo.guru99.com/V1/index.php");
+		 driver.get("https://demo.guru99.com/V1/index.php");
 		 
 		 
 		 
 		 
+		 
+		 
+		
 
 	}
 
