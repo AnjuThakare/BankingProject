0d5256e9185c72c0fe0cda0decddc8abebaf57c6
AnjuThakare <anju.thakare1989@gmail.com>
2023-01-21 17:17:18 -0500

    DGTL  DGTL-104:Login Page Test Case changes is done

---
+++ src/test/java/Com/bankingproject/loginpage/LoginPageTest.java
@@ -0,0 +1,44 @@
+package Com.bankingproject.loginpage;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Com.bankingproject.base.BaseClass;
+
+public class LoginPageTest extends BaseClass {
+
+	//WebDriver driver;
+		@BeforeClass
+		public void setUp() {
+			launchWebsite();
+		}
+		@AfterClass
+		public void tearDown() {
+			driver.close();
+			
+		}
+		@Test
+		public void testTitle() {
+			String pageTitle = driver.getTitle();
+			if(pageTitle.equalsIgnoreCase("GTPL Bank Home Page")) {
+				System.out.println("we are on correct home page");
+			}
+			else {
+				System.out.println("we are on wrong page");
+			}
+		}
+			@Test
+			public void testUrl() {
+				String pageTitle = driver.getCurrentUrl();
+				if(pageTitle.equalsIgnoreCase("https://demo.guru99.com/V1/index.php")) {
+					System.out.println(" we are on correct page");
+				}
+				else {
+					System.out.println("wrong page....");
+				}
+		}
+	
+	
+	
+}
